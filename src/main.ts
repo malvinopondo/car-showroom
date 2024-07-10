0676c21d239d48cb2798a1b42cf5c9e6f6ad5915
Malvin Opondo <malvin.opondo@student.moringaschool.com>
2024-07-10 11:46:41 +0300

    feat(Home): Create home component

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,12 @@
 import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
-
 import { AppModule } from './app/app.module';
+import { provideRouter } from '@angular/router';
+
 
 platformBrowserDynamic().bootstrapModule(AppModule, {
   ngZoneEventCoalescing: true
 })
   .catch(err => console.error(err));
+
+
+
